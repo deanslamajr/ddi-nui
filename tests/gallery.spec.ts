0bf58fce3b5ca42fde0c63c8ededcedcb91b7474
Dean Slama <deanslamajr@gmail.com>
2022-07-10 17:01:57 -0600

    finished first test!

--- tests/gallery.spec.ts
+++ tests/gallery.spec.ts
@@ -1,22 +1,58 @@
-import { test, expect, type Page } from "@playwright/test";
+import {
+  test,
+  expect,
+  type Page,
+  type Locator,
+  type JSHandle,
+} from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("localhost/v2/gallery");
 });
 
+const getComicUrlId = async (locator: Locator): Promise<string | undefined> => {
+  const element = await locator.elementHandle();
+  return (await element?.getProperty("id"))?.evaluate((id) => id);
+};
+
 test.describe("Gallery Page", () => {
   test("clicking a comic should navigate to that comic", async ({ page }) => {
-    // Click first comic in gallery
-    const firstComicOnGallery = await page.locator(
+    const firstComicOnInitialGalleryLoad = await page.locator(
+      ".comics-container a:first-child"
+    );
+    const lastComicOnInitialGalleryLoad = await page.locator(
+      ".comics-container a:last-child"
+    );
+
+    const firstComicOnInitialGalleryLoadUrlId = await getComicUrlId(
+      firstComicOnInitialGalleryLoad
+    );
+    const lastComicOnInitialGalleryLoadUrlId = await getComicUrlId(
+      lastComicOnInitialGalleryLoad
+    );
+
+    await lastComicOnInitialGalleryLoad.click();
+
+    await expect(page.url()).toContain(
+      `/comic/${lastComicOnInitialGalleryLoadUrlId}`
+    );
+
+    await page.locator("text=GALLERY").click();
+
+    const firstComicOnGalleryAfterReturn = await page.locator(
       ".comics-container a:first-child"
     );
 
-    const element = await firstComicOnGallery.elementHandle();
-    const id = await element?.getProperty("id");
+    const firstComicOnGalleryAfterReturnUrlId = await getComicUrlId(
+      firstComicOnGalleryAfterReturn
+    );
 
-    await firstComicOnGallery.click();
+    expect(firstComicOnGalleryAfterReturnUrlId).toEqual(
+      lastComicOnInitialGalleryLoadUrlId
+    );
 
-    // Make sure the list only has one todo item.
-    await expect(page.url()).toContain(`/comic/${id}`);
+    await page.locator("text=↑").click();
+
+    await page.locator(`.${firstComicOnInitialGalleryLoadUrlId}`);
   });
 });
