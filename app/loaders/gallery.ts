ef87416c38e4900ea2de94d5dfb1b6aa3b856f67
Dean Slama <deanslamajr@gmail.com>
2022-10-16 15:00:31 -0600

    fixed infinite render cycle on gallery component due to bad state and useEffect pattern

--- app/loaders/gallery.ts
+++ app/loaders/gallery.ts
@@ -21,6 +21,117 @@ export type LoaderData = {
   hasCursor: boolean;
   older: ComicsPagination;
   newer: ComicsPagination;
+  isSearch: boolean;
+};
+
+const fetchNewerComics = async ({
+  hasCursor,
+  newerOffset,
+}: {
+  hasCursor: boolean;
+  newerOffset: string;
+}): Promise<LoaderData> => {
+  let getNewerComicsResponse: {
+    comics: Comic[];
+    hasMoreNewer: boolean;
+  } | null = null;
+
+  try {
+    const response: Response = await fetch(
+      DDI_API_ENDPOINTS.getPreviousComics(newerOffset)
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    getNewerComicsResponse = await response.json();
+  } catch (error: any) {
+    // TODO better logging
+    console.error(error);
+  }
+
+  const comics: Comic[] = getNewerComicsResponse?.comics || [];
+  const hasComics = comics.length > 0;
+  const sortedComics = sortComics(comics);
+
+  return {
+    hasCursor,
+    newer: {
+      comics: sortedComics,
+      hasMore: getNewerComicsResponse?.hasMoreNewer || false,
+      cursor: hasComics ? sortedComics[0].updatedAt : null,
+    },
+    // will only use this data on page load requests
+    // to initialize the component state
+    older: {
+      hasMore: true,
+      cursor: hasComics
+        ? sortedComics[sortedComics.length - 1].updatedAt
+        : null,
+    },
+    isSearch: false,
+  };
+};
+
+const fetchOlderComics = async ({
+  captionSearch,
+  emojiFilter,
+  hasCursor,
+  offset,
+}: {
+  captionSearch?: string;
+  emojiFilter?: string;
+  hasCursor: boolean;
+  offset?: string;
+}): Promise<LoaderData> => {
+  let getComicsResponse: {
+    comics: Comic[];
+    cursor: string;
+    hasMoreOlder: boolean;
+  } | null = null;
+
+  try {
+    const response: Response = await fetch(
+      DDI_API_ENDPOINTS.getComics({
+        captionSearch,
+        emojiFilter,
+        offset,
+      })
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    getComicsResponse = await response.json();
+  } catch (error: any) {
+    // TODO better logging
+    console.error(error);
+  }
+
+  let earliestComicUpdatedAt;
+  const comics: Comic[] = getComicsResponse?.comics || [];
+  if (comics.length > 0) {
+    const earliestComic = comics[0];
+    earliestComicUpdatedAt = earliestComic.updatedAt;
+  }
+
+  return {
+    hasCursor,
+    older: {
+      comics,
+      hasMore: getComicsResponse?.hasMoreOlder || false,
+      cursor: getComicsResponse?.cursor || null,
+    },
+    // will only use this data on page load requests
+    // to initialize the component state
+    newer: {
+      cursor: earliestComicUpdatedAt || null,
+      hasMore: true,
+    },
+    isSearch: Boolean(captionSearch || emojiFilter),
+  };
 };
 
 const loader: LoaderFunction = async ({ request }) => {
@@ -36,114 +147,24 @@ const loader: LoaderFunction = async ({ request }) => {
 
     const hasCursor = Boolean(olderOffset || newerOffset);
 
-    let errorAttributes: Record<string, string> = {};
-
-    // This data request is asking for
     if (newerOffset) {
-      let getNewerComicsResponse: {
-        comics: Comic[];
-        hasMoreNewer: boolean;
-      } | null = null;
-
-      try {
-        const response: Response = await fetch(
-          DDI_API_ENDPOINTS.getPreviousComics(newerOffset)
-        );
-
-        if (!response.ok) {
-          errorAttributes = {
-            statusCode: response.status.toString(),
-            url: response.url,
-            statusText: response.statusText,
-          };
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        getNewerComicsResponse = await response.json();
-      } catch (error: any) {
-        // TODO better logging
-        console.error(error);
-      }
-
-      const comics: Comic[] = getNewerComicsResponse?.comics || [];
-      const hasComics = comics.length > 0;
-      const sortedComics = sortComics(comics);
-
-      return resolve(
-        json({
-          hasCursor,
-          newer: {
-            comics: sortedComics,
-            hasMore: getNewerComicsResponse?.hasMoreNewer || false,
-            cursor: hasComics ? sortedComics[0].updatedAt : null,
-          },
-          // will only use this data on page load requests
-          // to initialize the component state
-          older: {
-            hasMore: true,
-            cursor: hasComics
-              ? sortedComics[sortedComics.length - 1].updatedAt
-              : null,
-          },
-        })
-      );
-    }
-
-    let getComicsResponse: {
-      comics: Comic[];
-      cursor: string;
-      hasMoreOlder: boolean;
-    } | null = null;
-
-    try {
-      const response: Response = await fetch(
-        DDI_API_ENDPOINTS.getComics({
-          captionSearch,
-          emojiFilter,
-          offset: olderOffset,
-        })
-      );
-
-      if (!response.ok) {
-        errorAttributes = {
-          statusCode: response.status.toString(),
-          url: response.url,
-          statusText: response.statusText,
-        };
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      getComicsResponse = await response.json();
-    } catch (error: any) {
-      // TODO better logging
-      console.error(error);
-    }
-
-    let earliestComicUpdatedAt;
-    const comics: Comic[] = getComicsResponse?.comics || [];
-    if (comics.length > 0) {
-      const earliestComic = comics[0];
-      earliestComicUpdatedAt = earliestComic.updatedAt;
-    }
-
-    return resolve(
-      json({
+      const newerComicsResponse = await fetchNewerComics({
         hasCursor,
-        older: {
-          comics,
-          hasMore: getComicsResponse?.hasMoreOlder || false,
-          cursor: getComicsResponse?.cursor || null,
-        },
-        // will only use this data on page load requests
-        // to initialize the component state
-        newer: {
-          cursor: earliestComicUpdatedAt || null,
-          hasMore: true,
-        },
-      })
-    );
+        newerOffset,
+      });
+
+      return resolve(json(newerComicsResponse));
+    }
+
+    const olderComicsResponse = await fetchOlderComics({
+      captionSearch,
+      emojiFilter,
+      hasCursor,
+      offset: olderOffset,
+    });
+
+    return resolve(json(olderComicsResponse));
   });
-  // });
 };
 
 export default loader;
