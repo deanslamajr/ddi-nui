55252e403b8335a25f1964e7fc6de2cbf78396fc
Dean Slama <deanslamajr@gmail.com>
2023-01-18 21:12:55 -0700

    prototyping new approach to submenus

--- app/components/CellStudio/SizeMenu.tsx
+++ app/components/CellStudio/SizeMenu.tsx
@@ -3,17 +3,23 @@ import type { LinksFunction } from "@remix-run/node";
 import styled from "styled-components";
 import { useParams } from "@remix-run/react";
 import { GiResize } from "react-icons/gi";
+import cloneDeep from "fast-clone";
 
 import { EMOJI_CONFIG } from "~/utils/constants";
 import { theme } from "~/utils/stylesTheme";
 
+import { EmojiConfigSerialized } from "~/models/emojiConfig";
+
 import { useComicStudioState } from "~/contexts/ComicStudioState";
 import { resizeEmoji } from "~/contexts/ComicStudioState/actions";
-import { getEmojiSize } from "~/contexts/ComicStudioState/selectors";
+// import { getEmojiSize } from "~/contexts/ComicStudioState/selectors";
 
 import { MenuButton, links as buttonStylesUrl } from "~/components/Button";
 import Slider, { links as sliderStylesUrl } from "~/components/Slider";
 import DPad, { links as dPadStylesUrl } from "~/components/DPad";
+import EmojiCanvas, {
+  links as emojiCanvasStylesUrl,
+} from "~/components/EmojiCanvas";
 
 import { BackMenuButton } from "./MainMenu";
 
@@ -21,6 +27,7 @@ import stylesUrl from "~/styles/components/CellStudio.css";
 
 export const links: LinksFunction = () => {
   return [
+    ...emojiCanvasStylesUrl(),
     ...buttonStylesUrl(),
     ...sliderStylesUrl(),
     ...dPadStylesUrl(),
@@ -41,18 +48,70 @@ const Label = styled.span`
 `;
 
 const SizeMenu: React.FC<{
+  activeEmojiId: number;
+  emojiConfigs: Record<string, EmojiConfigSerialized>;
+  handleDragEnd: (args: { xDiff: number; yDiff: number }) => void;
+  backgroundColor: string;
   onBackButtonClick: () => void;
-}> = ({ onBackButtonClick }) => {
+}> = ({
+  activeEmojiId,
+  backgroundColor,
+  emojiConfigs,
+  handleDragEnd,
+  onBackButtonClick,
+}) => {
   const params = useParams();
   const cellUrlId = params.cellUrlId!;
 
+  const [state, setState] = React.useState<{
+    emojiSize: number;
+    localEmojiConfigs: EmojiConfigSerialized[];
+  }>(() => {
+    const localEmojiConfigs = Object.values(emojiConfigs);
+    const activeEmoji = emojiConfigs[activeEmojiId];
+    return {
+      emojiSize: activeEmoji.size,
+      localEmojiConfigs,
+    };
+  });
+  // const [activeEmoji] = React.useState(emojiConfigs[activeEmojiId]);
+
+  const resizeLocalEmoji = (newSize: number): void => {
+    const clonedEmojiConfigs = cloneDeep(state.localEmojiConfigs);
+    const clonedActiveEmoji = clonedEmojiConfigs.find(
+      (e) => e.id === activeEmojiId
+    ); //clonedEmojiConfigs[activeEmojiId];
+    if (!clonedActiveEmoji) {
+      return;
+    }
+    clonedActiveEmoji.size = newSize;
+    setState({ emojiSize: newSize, localEmojiConfigs: clonedEmojiConfigs });
+  };
+
   const [comicStudioState, dispatch] = useComicStudioState();
-  const emojiSize = getEmojiSize(comicStudioState, cellUrlId);
+  const saveAndGoBack = () => {
+    dispatch(
+      resizeEmoji({
+        newSize: state.emojiSize,
+        cellUrlId,
+        shouldSaveChange: true,
+      })
+    );
+    onBackButtonClick();
+  };
+
+  // const emojiSize = getEmojiSize(comicStudioState, cellUrlId);
 
   return (
     <>
-      <BackMenuButton onBackButtonClick={onBackButtonClick} />
-      {emojiSize !== null ? (
+      <EmojiCanvas
+        activeEmojiId={activeEmojiId}
+        backgroundColor={backgroundColor}
+        emojiConfigs={state.localEmojiConfigs}
+        handleDragEnd={handleDragEnd}
+      />
+      <BackMenuButton onBackButtonClick={saveAndGoBack} />
+      {state.emojiSize !== null ? (
         <SliderContainer>
           <Label>
             <GiResize color={theme.colors.pink} size="2rem" />
@@ -60,45 +119,54 @@ const SizeMenu: React.FC<{
           <Slider
             min={EMOJI_CONFIG.MIN_SIZE}
             max={EMOJI_CONFIG.MAX_SIZE}
-            step={1}
-            value={emojiSize}
-            onChange={(value) =>
-              dispatch(
-                resizeEmoji({
-                  newSize: value,
-                  cellUrlId,
-                  shouldSaveChange: false,
-                })
-              )
+            step={10}
+            value={state.emojiSize}
+            onChange={
+              (value) => resizeLocalEmoji(value)
+              // dispatch(
+              //   resizeEmoji({
+              //     newSize: value,
+              //     cellUrlId,
+              //     shouldSaveChange: false,
+              //   })
+              // )
             }
-            onRelease={(value) =>
-              dispatch(
-                resizeEmoji({
-                  newSize: value,
-                  cellUrlId,
-                  shouldSaveChange: true,
-                })
-              )
+            onRelease={
+              (value) => resizeLocalEmoji(value)
+              // dispatch(
+              //   resizeEmoji({
+              //     newSize: value,
+              //     cellUrlId,
+              //     shouldSaveChange: true,
+              //   })
+              // )
             }
           />
           <DPad
             horizontalActions={(multiplier) => ({
-              onLeftClick: () =>
-                dispatch(
-                  resizeEmoji({
-                    newSize: emojiSize - multiplier,
-                    cellUrlId,
-                    shouldSaveChange: true,
-                  })
-                ),
+              onLeftClick: () => {
+                let newValue = state.emojiSize - multiplier;
+                if (newValue <= 0) {
+                  newValue = 0;
+                }
+                resizeLocalEmoji(newValue);
+              },
+              // dispatch(
+              //   resizeEmoji({
+              //     newSize: emojiSize - multiplier,
+              //     cellUrlId,
+              //     shouldSaveChange: true,
+              //   })
+              // ),
               onRightClick: () =>
-                dispatch(
-                  resizeEmoji({
-                    newSize: emojiSize + multiplier,
-                    cellUrlId,
-                    shouldSaveChange: true,
-                  })
-                ),
+                resizeLocalEmoji(state.emojiSize + multiplier),
+              // dispatch(
+              //   resizeEmoji({
+              //     newSize: emojiSize + multiplier,
+              //     cellUrlId,
+              //     shouldSaveChange: true,
+              //   })
+              // ),
             })}
           />
         </SliderContainer>
