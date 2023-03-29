767a60796050a75d3bc018f60d5ed9a3bf3497ba
Dean Slama <deanslamajr@gmail.com>
2023-03-28 20:11:35 -0600

    began fleshing out reducer for updating state

---
+++ app/contexts/ComicStudioState/reducer/updateCellCaption.ts
@@ -0,0 +1,32 @@
+import cloneDeep from "fast-clone";
+
+import { ComicStudioStateReducer, UpdateCellCaptionAction } from "../types";
+import { getActiveEmoji, getCellState } from "../selectors";
+import { addNewCellChangeToHistory } from "~/models/cellChange";
+
+const updateCellCaption: ComicStudioStateReducer<UpdateCellCaptionAction> = (
+  state,
+  action
+) => {
+  // try {
+  //   const clonedState = cloneDeep(state);
+  //   const activeEmoji = getActiveEmoji(clonedState, action.data.cellUrlId);
+  //   if (!activeEmoji) {
+  //     throw new Error("Active Emoji not found!");
+  //   }
+  //   activeEmoji.opacity = action.data.newOpacity;
+  //   const cellState = getCellState(clonedState, action.data.cellUrlId);
+  //   if (!cellState || !cellState.studioState) {
+  //     throw new Error("Cell state not found!");
+  //   }
+  //   cellState.isDirty = true;
+  //   cellState.hasNewImage = true;
+  //   addNewCellChangeToHistory(cellState);
+  //   return clonedState;
+  // } catch (e) {
+  //   console.error(e);
+  //   return state;
+  // }
+};
+
+export default updateCellCaption;
